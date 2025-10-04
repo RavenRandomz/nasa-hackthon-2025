370455d43485d38a46df4e46d095ea4a1786d267
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-04 17:39:00 -0500

    feat(Door): Create continuity detection means

---
+++ scene/door/door.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+class_name Door
+
+signal door_connected()
+signal door_disconnected()
+
+@onready var parent = get_parent()
+
+var _connected_door_count = 0
+
+func _connected_check():
+	if _connected_door_count == 0:
+		door_connected.emit()
+		print("Connected")
+		
+func _disconnected_check():
+	if _connected_door_count == 0:
+		door_disconnected.emit()
+		print("Disconnected")
+
+func _on_area_entered(area: Area2D) -> void:
+	if (typeof(area) == typeof(Door)):
+		_connected_door_count += 1
+		_connected_check()
+
+func _on_area_exited(area: Area2D) -> void:
+	if (typeof(area) == typeof(Door)):
+		_connected_door_count -= 1
+		_disconnected_check()
