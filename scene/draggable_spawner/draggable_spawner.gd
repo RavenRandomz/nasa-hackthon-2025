39124e45961bd47de39fc3388f8f60445f340254
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-04 15:34:11 -0500

    fix(DraggableSpawner): Prevent off-center spawning which interferes with mouse

--- scene/draggable_spawner/draggable_spawner.gd
+++ scene/draggable_spawner/draggable_spawner.gd
@@ -4,13 +4,19 @@ extends Button
 @export var _scene_to_spawn : PackedScene
 @export var _spawn_into : Node
 
+func _get_global_mouse_pos():
+	var viewport_origin = get_viewport_transform().get_origin()
+	var viewport_mouse_pos = get_viewport().get_mouse_position()
+	var global_mouse_pos = viewport_origin + viewport_mouse_pos
+	return global_mouse_pos
+
 func _on_pressed() -> void:
 	pass
 
 func _on_button_down() -> void:
 	var spawned : Draggable = _scene_to_spawn.instantiate() 
-	spawned.global_position = global_position
 	_spawn_into.add_child(spawned)
+	var center = spawned.get_rect().size / 2
+	spawned.global_position = _get_global_mouse_pos() - center
 	spawned._on_button_down()
-	pass # Replace with function body.
  # Replace with function body.
