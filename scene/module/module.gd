8ae796869dbbb26e9b753f18ba515c28d48e9dea
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-04 18:28:49 -0500

    feat(Module): Add integration with doors

---
+++ scene/module/module.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+class_name Module
+
+var _ports: Array[Port]
+
+var _connected_port_count = 0
+
+func _ready():
+	for child in get_children():
+		if (child is Port):
+			_ports.push_back(child)
+		for port in _ports:
+			port.port_connected.connect(_on_port_connected)
+			port.port_disconnected.connect(_on_port_disconnected)
+
+func _on_port_connected():
+	_connected_port_count += 1
+	print(_connected_port_count)
+
+func _on_port_disconnected():
+	_connected_port_count -= 1
+	print(_connected_port_count)
