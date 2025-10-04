07edb2f8e51968fc80cde6e1c4ccdb2c45d220d9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-04 18:25:40 -0500

    fix(port): Align to class name

--- scene/port/port.gd
+++ scene/port/port.gd
@@ -1,31 +1,31 @@
 extends Area2D
 class_name Port
 
-signal door_connected()
-signal door_disconnected()
+signal port_connected()
+signal port_disconnected()
 
 @onready var parent = get_parent()
 
-var _connected_door_count = 0
+var _connected_port_count = 0
 
 func _connected_check():
 	# Prevent repeats
-	if _connected_door_count == 1:
-		door_connected.emit()
+	if _connected_port_count == 1:
+		port_connected.emit()
 		print("Connected")
 		
 func _disconnected_check():
 	# Prevent false alarms if multiple intersections
-	if _connected_door_count == 0:
-		door_disconnected.emit()
+	if _connected_port_count == 0:
+		port_disconnected.emit()
 		print("Disconnected")
 
 func _on_area_entered(area: Area2D) -> void:
 	if (area is Port):
-		_connected_door_count += 1
+		_connected_port_count += 1
 		_connected_check()
 
 func _on_area_exited(area: Area2D) -> void:
 	if (area is Port):
-		_connected_door_count -= 1
+		_connected_port_count -= 1
 		_disconnected_check()
