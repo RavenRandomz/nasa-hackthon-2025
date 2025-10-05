746f06216a6539d49272ce4b32f4d3a9c191f967
Jose Galindo <gal_joseluis@hotmail.com>
2025-10-05 04:37:45 -0500

    build(UI): more menus

--- scripts/main_menu_ui.gd
+++ scripts/main_menu_ui.gd
@@ -1,13 +1,19 @@
 extends CanvasLayer
 
-func _on_exit_button_pressed() -> void:
-	get_tree().quit()
-
-func _on_learn_more_button_pressed() -> void:
-	print("'Learn more button' pressed")
-
-func _on_instructions_button_pressed() -> void:
-	print("'Instructions' button pressed")
+@onready var modeSelectScreen = "res://scenes/UI/mode_selector_screen.tscn"
+@onready var instructionsScreen = ""
+@onready var learnMoreScreen = ""
 
 func _on_start_button_pressed() -> void:
-	print("'Start button pressed'")
+	get_tree().change_scene_to_file(modeSelectScreen)
+
+func _on_instructions_button_pressed() -> void:
+	get_tree().change_scene_to_file(instructionsScreen)
+
+func _on_learn_more_button_pressed() -> void:
+	get_tree().change_scene_to_file(learnMoreScreen)
+
+#TODO: Community Button
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
