a7e8f6b5f035c24ab673438464ad518463be477a
Jose Galindo <gal_joseluis@hotmail.com>
2025-10-05 03:07:44 -0500

    fix(dir): scripts migration

--- home_button.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-func _ready():
-	var button = Button.new()
-	button.pressed.connect(_button_pressed)
-	add_child(button)
-
-func _button_pressed():
-	
---
+++ scripts/generic_info_menu_ui.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+func _on_home_button_pressed() -> void:
+	print("HomeButton Pressed")
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
+
+func _on_right_arrow_button_pressed() -> void:
+	print("rightArrowButton Pressed")
+
+func _on_left_arrow_button_pressed() -> void:
+	print("leftArrowButton Pressed")
